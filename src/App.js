9c2c3f7b98b732e696348dd1bc9a04daf9044964
WANG <wybjohnmacdonald@outlook.com>
2020-02-25 11:07:13 +0100

    positionner2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import './App.css';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet'
 
  
 
@@ -10,8 +11,8 @@ class MyMap extends React.Component {
   constructor (prpos) {
     super(prpos)
     this.state = {
-      lat:49.1191,
-      lng:6.1727,
+      lat:0,
+      lng:0,
       zoom: 16
     }
     //this.displayLocationInfo=this.displayLocationInfo.bind(this);
@@ -37,19 +38,47 @@ class MyMap extends React.Component {
     alert("Sorry, browser does not support geolocation!");
   }
 }
- 
+refreshloc(){
+  const watcher = navigator.geolocation.watchPosition(this.displayLocationInfo);
+  setTimeout(() => {
+    navigator.geolocation.clearWatch(watcher);
+  }, 1000);
+}
+
+
 
   render () {
+   const markerimg = new L.Icon({
+
+      iconUrl: 'mr.svg',
+    
+      iconRetinaUrl: 'mr.svg',
+    
+      iconAnchor: [20, 40],
+    
+      popupAnchor: [0, -35],
+    
+      iconSize: [40, 40],
+    
+      shadowUrl: null,
+    
+      shadowSize:null,
+    
+      shadowAnchor: null,
+    
+    })
+
     this.getLocation();
+    this.refreshloc();
     const pos = [this.state.lat, this.state.lng]
     return (
       <div className="map-container">
         <Map style={{width: '100%',height: '800px'}} center={pos} zoom={this.state.zoom}>
           <TileLayer
-            url='http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={pos}>
+          <Marker position={pos} icon={markerimg}>
             <Popup>
               <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
             </Popup>
